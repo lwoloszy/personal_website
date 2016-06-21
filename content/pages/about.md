ec77d8d4788ab927d156aa3593a06f7e41145649
Luke Woloszyn <luke.woloszyn@gmail.com>
2016-06-20 17:21:44 -0700

    initial commit

---
+++ content/pages/about.md
@@ -0,0 +1,10 @@
+Title: About
+
+After 10 years of academic research in neuroscience, I am pivoting my career towards doing data science in industry. I look forward to using quantitative and computational techniques to help companies discover data driven insights.
+
+I currently live in beautiful Seattle, WA, with my gorgeous wife Sara, both us having recently relocated from NYC. While we loved our time in the urban jungle, and will dearly miss our favorite bagel, falafel and pizza places, we very much look forward to exploring the amazing outdoor opportunities that the Pacific NW offers.
+
+When I am not coding or learning about things data science related, I can usually be found running, hiking, at a concert near you, or relaxing at home. Having grown up in Dallas during the Cowboys glory days, I am an unabashed fan of all Dallas sports teams. I'm less a fan of the political climate in Texas.
+
+
+
---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Luke Woloszyn'
+SITEURL = u'http://lukewoloszyn.com'
+SITENAME = u'Luke Woloszyn'
+SITESUBTITLE = u'data scientist, outdoor enthusiast, music lover, beer snob'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+RELATIVE_URLS = True
+
+TYPOGRIFY = True
+
+THEME = '../crowsfoot'
+
+# PATH = 'content'
+ARTICLE_PATHS = ['blog']
+
+# MENUITEMS = [('blog', '/'), ('cv', '/misc/cv.pdf')]
+MENUITEMS = [('Blog', '/'), ]
+PROFILE_IMAGE_URL = '/images/personal.jpg'
+
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'misc']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+}
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+DEFAULT_PAGINATION = None
+
+# addresses
+EMAIL_ADDRESS = 'luke.woloszyn@gmail.com'
+GITHUB_ADDRESS = 'https://github.com/lwoloszy'
+LINKEDIN_ADDRESS = 'https://www.linkedin.com/in/lukewoloszyn'
+TWITTER_ADDRESS = 'https://www.twitter.com/lukewoloszyn'
+
+# feed
+FEED_RSS = 'feeds/rss.xml'
+FEED_MAX_ITEMS = 10
+
+SHOW_ARTICLE_AUTHOR = False
+
+LICENSE_NAME = "CC BY-SA"
+LICENSE_URL = "https://creativecommons.org/licenses/by-sa/3.0/"
+
+LOAD_CONTENT_CACHE = False
